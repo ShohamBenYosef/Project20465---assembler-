0703c93334a1498567ccec7b0d58d0e78fde4679
yairamit <62564451+yairamit@users.noreply.github.com>
2021-03-21 16:31:39 +0200

    Update commands.h

--- commands.h
+++ commands.h
@@ -1,49 +1,93 @@
-typedef struct
-{
-    char* name; /* Command name*/
-    int opcode; /* Command opcode */
-    int funct; /* Command function code */
-    int numOfOpreand; /* Number of operands expected with command */
-    unsigned int srcImidiateadAdress : 1;
-    unsigned int srcDirectAddress : 1;
-    unsigned int srcRelativeAddress : 1;
-    unsigned int srcDirectRegAdress : 1;
-    unsigned int destImidiateAddress : 1;
-    unsigned int destDirectAddress : 1;
-    unsigned int destRelativeAddress : 1;
-    unsigned int destDirectRegAdress : 1;
+#ifndef COMMANDS_H_
+#define COMMANDS_H_
 
-} commandsType;
+#include "utils.h"
+#define numOfcommands 15
+#define lengthOfCommands 3
+#define NewCommandLine(cmd) cmd = (Command*)malloc(sizeof(Command));\
+	if (!cmd) fatal_error(ErrorMemoryAlloc);\
+	cmd->first_operand = NULL;\
+	cmd->second_operand = NULL;\
+	cmd->numOfCommand = 0;\
+	cmd->address = 0;
 
 typedef enum
 {
-	immediateNum,
-	SymbolOperand
+	immediateOperand,
+	SymbolOperand,
 	RealativeOperand,
 	RegisterOperand
 } operandType;
 
-typedef struct
-{
-	operandType type; /* Type of operand */
-	union 
+typedef union
 	{
 		int immediateNum; /* Number for immediate addres */
 		char* Symbol; /* If there is a symbol in the operand */
 		char numOfRegister; /* Number of register in operand - for register type */
-	} operandValue; /* What inside the operand */
+} operandValue; /* What inside the operand */
+
+
+
+typedef struct
+{
+    	operandType type; /* Type of operand */
+    	union
+    	{
+    		int immediateNum; /* Number for immediate addres */
+    		char* Symbol; /* If there is a symbol in the operand */
+    		int numOfRegister; /* Number of register in operand - for register type */
+    } operandValue; /* What inside the operand */
 } operand;
 
-
-
-
 /*
- struct of a command  with all the deatils
+ Struct of a command  with all the deatils
  */
 typedef struct
 {
-    int numOfCommand; /* number of the command from the commandsTypes */
-	operand firstOperand; /* origin operand */
-	operand secondOperand; /* destination operand */
+	int numOfCommand; /* number of the command from the commandsTypes */
+	operand FirstOperand; /* origin operand */
+	operand SecondOperand; /* destination operand */
 	int address; /* the address of command */
 } command;
+
+
+
+
+
+typedef struct
+{
+	char* name; /* Command name*/
+	int opcode; /* Command opcode */
+	int funct; /* Command function code */
+	int numOfOpreand; /* Number of operands expected with command */
+	unsigned int srcImidiateadAdress : 1;
+	unsigned int srcDirectAddress : 1;
+	unsigned int srcRelativeAddress : 1;
+	unsigned int srcDirectRegAdress : 1;
+	unsigned int destImidiateAddress : 1;
+	unsigned int destDirectAddress : 1;
+	unsigned int destRelativeAddress : 1;
+	unsigned int destDirectRegAdress : 1;
+
+} CommandsType;
+
+
+
+
+
+
+command parseCommand(char* line);
+
+operand getFirstOperand(char* currentChar);
+
+int typeOfCommand(char* word);
+
+operand parseOperand(char* currentChar);
+
+operand getSecondOperand(char* line);
+
+/*void pushCommandToTable(command lineCommand, Line newLine, int addres);*/
+
+int checkOperands(const command* newCommand);
+
+#endif
